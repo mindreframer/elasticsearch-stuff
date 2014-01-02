21198449a2cedd7f5192def2a59c36b933cfcf25
Roman Heinrich <roman.heinrich@gmail.com>
2014-01-02 11:36:53 +0100

    Squashed 'github.com/elbii/plunk/' changes from 500029f..6371fc7
    6371fc7 remove hard-coded date from gemspec ;-)
    89fd00a size should be an option
    b78cfbd use elasticsearch gem
    2d5dfec refactoring to use official ES ruby lib
    5db2856 escape query in query validator
    e1d2ea1 remove legacy rudimentary query builder
    5d6851f add AUTHORS
    
    git-subtree-dir: github.com/elbii/plunk
    git-subtree-split: 6371fc76f2e3aa520f4031d66151a7f3c85abfee

--- lib/plunk.rb
+++ lib/plunk.rb
@@ -1,3 +1,36 @@
-class Plunk
+require 'elasticsearch'
 
+require 'plunk/utils'
+require 'plunk/parser'
+require 'plunk/transformer'
+require 'plunk/result_set'
+
+module Plunk
+  class << self
+    attr_accessor :elasticsearch_options, :elasticsearch_client,
+      :parser, :transformer, :max_number_of_hits
+  end
+
+  def self.configure(&block)
+    class_eval(&block)
+    initialize_parser
+    initialize_transformer
+    initialize_elasticsearch
+  end
+
+  def self.initialize_elasticsearch
+    self.elasticsearch_client ||= Elasticsearch::Client.new(elasticsearch_options)
+  end
+
+  def self.initialize_parser
+    self.parser ||= Parser.new
+  end
+
+  def self.initialize_transformer
+    self.transformer ||= Transformer.new
+  end
+
+  def self.search(query_string)
+    transformer.apply(parser.parse(query_string)).eval
+  end
 end
